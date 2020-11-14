397c5ce928c01818930ed3527363f83888194a93
Fagun <mdabuzaforfagun@gmail.com>
2020-11-14 10:52:29 +0600

    Add project files.

---
+++ Domain/RazorHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace Domain
+{
+    public class RazorHelper : IRazorHelper
+    {
+        private readonly IRazorViewEngine _viewEngine;
+
+        public RazorHelper(IRazorViewEngine viewEngine)
+        {
+            _viewEngine = viewEngine;
+        }
+        public void FindView()
+        {
+            try
+            {
+            }
+            catch (CompilationFailedException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+
+    public interface IRazorHelper
+    {
+        void FindView();
+    }
+}
---
+++ WebAPI/Controllers/ViewsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Domain;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ViewsController : ControllerBase
+    {
+        private readonly IRazorHelper _helper;
+
+        public ViewsController(IRazorHelper helper)
+        {
+            _helper = helper;
+        }
+
+        [HttpGet]
+        public IActionResult ProduceException()
+        {
+            _helper.FindView();
+
+            return Ok();
+        }
+    }
+}
